4e29052379de663ef4bba92f2ae35fabfaa8ba11
xiaoyang2016 <yangxiao@Objectivasoftware.com>
2017-02-28 18:57:02 +0800

    ok
    Signed-off-by: xiaoyang2016 <yangxiao@Objectivasoftware.com>

---
+++ app/src/main/java/com/wehealth/pdqbook/getway/model/SearchResult.java
@@ -0,0 +1,11 @@
+package com.wehealth.pdqbook.getway.model;
+
+/**
+ * @Author yangxiao on 2/28/2017.
+ */
+
+public class SearchResult {
+    private boolean isSuccess;
+    private String message;
+    private int error;
+}
---
+++ app/src/main/java/com/wehealth/pdqbook/getway/model/SearchResultData.java
@@ -0,0 +1,9 @@
+package com.wehealth.pdqbook.getway.model;
+
+/**
+ * @Author yangxiao on 2/28/2017.
+ */
+
+public class SearchResultData {
+
+}
